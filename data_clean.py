1e1653a3381cbf2a4e4f8f153999243bae641635
Lambda-Analytica <164220003+Lambda-Analytica@users.noreply.github.com>
2024-05-02 08:58:18 -0400

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -58,5 +58,90 @@ unclean_df.head(10)
 print(unclean_df.info())
 print(unclean_df.describe())
 
+# Get all unique values from the 'SOS (TecDoc)' column
+unique_values = unclean_df['SOS (TecDoc)'].unique()
+
+# Print the unique values
+#print(unique_values_sos)
+
+# If you want to further inspect or manipulate the unique values, you can convert them to a list
+unique_values_list = list(unique_values)
+print(unique_values_list)
+
+# Data type conversions
+num_columns_to_convert = [
+    "Global Sales Price Class",
+    "K-Typ",
+    "N-Typ",
+    "TecDoc Engine Number",
+    "Count_2023",
+    "Count_2024",
+    "Count_2025",
+    "Count_2026",
+    "Count_2027",
+    "Count_2028",
+    "Count_2029",
+    "Count_2030",
+    "Count_2031",
+    "Count_2032",
+    "Count_2033"
+]
+for column in num_columns_to_convert:
+    unclean_df[column] = pd.to_numeric(unclean_df[column], errors='coerce')
+
+str_columns_to_convert = [
+    "Country/Territory",
+    "ISO-Code",
+    "Global Sales Segment",
+    "Global Sales Sub-Segment",
+    "Global Sales Price Class",
+    "MHCV Segmentation",
+    "Registration Type",
+    "Make Group",
+    "Make",
+    "Model Group",
+    "Model",
+    "Model-short",
+    "Body Group",
+    "Body Type",
+    "Fuel Type",
+    "Cylinder Conf.",
+    "Fuel System Type text (TecDoc)",
+    "Engine Code",
+    "Age"
+]
+for column in str_columns_to_convert:
+    unclean_df[column] = unclean_df[column].astype(str)
+
+# convert to datetime
+#conversion function for 'last available date' column
+def convert_last_available_date(date_str):
+    try:
+        return pd.to_datetime(date_str, format='%m/%Y')
+    except:
+        return pd.NaT
+
+#conversion function for 'year of first registration' column
+def convert_year_of_first_registration(year):
+    try:
+        return pd.to_datetime(year, format='%Y')
+    except:
+        return pd.NaT
+
+#conversion function for 'TecDoc' columns
+def convert_tecdoc(date_str):
+    try:
+        return pd.to_datetime(date_str, format='%Y%m')
+    except:
+        return pd.NaT
+
+# Apply conversion functions to DataFrame
+unclean_df['Last Available Date'] = unclean_df['Last Available Date'].apply(convert_last_available_date)
+unclean_df['Year of first registration'] = unclean_df['Year of first registration'].apply(convert_year_of_first_registration)
+unclean_df['SOS (TecDoc)'] = unclean_df['SOS (TecDoc)'].apply(convert_tecdoc)
+unclean_df['EOS (TecDoc)'] = unclean_df['EOS (TecDoc)'].apply(convert_tecdoc)
+
+# Print the updated DataFrame
+unclean_df.head(20)
 
 
